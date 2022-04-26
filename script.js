4f335ffb0a072c18acb9410cc64b8352f1fc8007
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-04-25 23:09:35 -0400

    WIP

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const player = document.querySelector('.player');
-const video =player.querySelector('.viewer');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('.[data-skip]');
+//const skipButtons = player.querySelector('.[data-skip]');
 const ranges = player.querySelectorAll('.player__slider')
 
 function togglePlay() {
-    if(video.paused) {
+    if (video.paused) {
         video.play();
     } else {
         video.pause();
     }
 }
 
+function updateButton() {
+    if (this.paused === true) {
+        `►`
+    } else {
+        `❚ ❚`
+    }
+    toggle.textContent = icon;
+    const icon = this;
+    console.log(icon)
+}
+
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', togglePlay);
+video.addEventListener('pause', togglePlay);
+toggle.addEventListener('click', togglePlay);
