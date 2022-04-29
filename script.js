4704e0cb76ef7177f8537a554db36fa1d4e350bf
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-04-29 01:05:13 -0400

    Complete skip function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider')
 
 function togglePlay() {
@@ -17,7 +17,6 @@ function updateButton() {
 }
 
 function skip() {
-  console.log(this.dataset.skip);
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
