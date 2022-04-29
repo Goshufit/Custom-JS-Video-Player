386988d2264b0fafc079f7d918592f861c86b6e1
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-04-28 23:49:51 -0400

    Finalize pause and play functions as well as icon toggle function begin skip function to skip though video

--- script.js
+++ script.js
@@ -3,19 +3,26 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-//const skipButtons = player.querySelector('.[data-skip]');
+const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider')
 
 function togglePlay() {
    const method = video.paused ? 'play' : 'pause';
    video[method]();
-};
+}
 
 function updateButton() {
-  const icon =this.pause ? ''
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
 }
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
